ea47ddbda6ec562cf504186d77830351acb3ed85
blizana <bryandelao@gmail.com>
2018-06-08 22:15:51 -0500

    Añadir iframe :)

--- page.php
+++ page.php
@@ -7,6 +7,22 @@ require_once('./include.php');
 
 $html_anime = file_get_contents('https://animeflv.net'.$_REQUEST['url']) ;
 // Zippyshare
+// Sacar los iframe
+$html_anime_iframe = $html_anime;
+$endf = true;
+
+for ($i=0; $i < 10 ; $i++) { 
+    $if = strpos($html_anime_iframe,'<iframe');
+    $iframe_string = substr($html_anime_iframe,$if);
+    $endf = strpos($iframe_string,"</iframe>");
+    $iframe_string  = substr($html_anime_iframe,$if,$endf + 9 );
+
+    $iframe[] =$iframe_string;
+    $html_anime_iframe  =str_replace( $iframe_string,'', $html_anime_iframe);
+}
+ 
+
+
 $final = removeScript(removeScript($html_anime),'<noscript','</noscript>');
 
 $end = true;
@@ -23,16 +39,14 @@ while ($end ) {
     if (strpos($link_clear,'http://ouo.io/s/y0d65LCP?s=')) {
            try {
                 $a = new SimpleXMLElement($link_clear);
-
                 $url_clean = str_replace('http://ouo.io/s/y0d65LCP?s=' , '',$a['href']);
-                $url_clean = urldecode($url_clean);
-                
-                $link_clear = '<a href="paso_final?url='.$url_clean .'">'.$url_clean .'</a>'.'======<a href="'.$url_clean .'" target="__black">Click To link</a>' ;
-                
+                $url_clean = urldecode($url_clean);                
+                $link_clear = '<a href="paso_final?url='.$url_clean .'">'.$url_clean .'</a>'.'======<a href="'.$url_clean .'" target="__black">Click To link</a>' ;                
             } catch (Exception $e) {
             
             } 
     }
+
 
     $link .= $link_clear .'<br>' ;
     $final  = substr($final,$i+$end);
@@ -42,6 +56,10 @@ $final = str_replace('/ver/','page.php?url=/ver/',$link);
 $final = str_replace('/anime/','interna.php?url=/anime/',$final);
 
 echo $final;
+
+foreach ($iframe as $row) {
+   echo $row;
+}
 
 // echo '<pre>'; var_dump( ($final) ); echo '</pre>'; die;/***HERE***/ 
 ?>
--- paso_final.php
+++ paso_final.php
@@ -1,39 +1,18 @@
 <?php 
 
-function converter_link(String $var = null)
-{
-    
-    $i = strpos($var,'="');
-    $var_resto = substr($var,$i);
-    $end = strpos($var_resto,'">');
-    if ($end) {
-        $url_clean = str_replace('" class="fa-play-circle' , '',substr($var,$i +2,$end -2 ));
 
-        return '<a href="interna.php?url='.$url_clean.'">'.$url_clean.'</a>';
-
-    }    else {
-        return $var;
-    }
-  
-}
 
 $html_anime = file_get_contents($_REQUEST['url']) ;
 $final = str_replace( 'script','',$html_anime);
-// $final = strip_tags( $html_anime);
-// EN EMISION
-// $i = strpos($final,'<ul class="ListSdbr">');
-// $string_final = substr($final,$i);
-// $end = strpos($string_final,"</ul>");
-
 $end = true;
 
 $ini = 0;
 $link = "";
-    
+
     $i = strpos($final,'[url=//');
     $string_final = substr($final,$i);
     $end = strpos($string_final,"[/url]");
-    $link_clear = converter_link(substr($final,$i + 7,$end - 7));
+    $link_clear = (substr($final,$i + 7,$end - 7));
 
 $i = strpos($final,'%1000');
 $string_final = substr($final,$i - 6);
