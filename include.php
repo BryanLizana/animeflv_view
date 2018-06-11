90a24627c5cce3cb6ee9d3ce294cb4cae5938382
BryanLizana <bryandelao5@gmail.com>
2018-06-11 10:10:41 -0500

    Ver luego

--- include.php
+++ include.php
@@ -3,4 +3,11 @@ require_once('./header.php');
 require_once('./class.anime.php');
 
 $AnimeFlv =  new AnimeFlv();
+
+// preg_match_all("|<[^>]+>(.*)</[^>]+>|U",
+//     "<b>ejemplo: </b><div align=\"left\">esto es una prueba</div>",
+//     $salida, PREG_SET_ORDER);
+// echo $salida[0][0] . ", " . $salida[0][1] . "\n";
+// echo $salida[1][0] . ", " . $salida[1][1] . "\n";
+
 ?> 
